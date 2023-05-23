7d0899999a011252ec76c3d3ddf4b13edcb6e88b
Will Couture <wcouture17@gmail.com>
2023-05-22 23:40:35 -0400

    Remove request for package downloads
    Will be run offline

--- __init__.py
+++ __init__.py
@@ -750,14 +750,14 @@ class Translator(MycroftSkill):
         to_code = langCode
 
         # Download and install Argos Translate package
-        argostranslate.package.update_package_index()
-        available_packages = argostranslate.package.get_available_packages()
-        package_to_install = next(
-            filter(
-                lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
-            )
-        )
-        argostranslate.package.install_from_path(package_to_install.download())
+        #argostranslate.package.update_package_index()
+        #available_packages = argostranslate.package.get_available_packages()
+        #package_to_install = next(
+        #    filter(
+        #        lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
+        #    )
+        #)
+        #argostranslate.package.install_from_path(package_to_install.download())
 
         # Translate
         translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
